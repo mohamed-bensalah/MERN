6aa9911b1642c30ce85d66973a1a8668ace43aa8
mohamed-bensalah <118818833+mohamed-bensalah@users.noreply.github.com>
2023-01-24 10:36:45 +0100

    First React With JSX

---
+++ Week1/Day2/Practice/First React With JSX/jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="https://www.codingdojo.com/">Hello Dojo</a>
+      <h2>Things I need to do:</h2>
+      <h5>* Learn React</h5>
+      <h5>* Climb Mt. Everest</h5>
+      <h5>* Run a marathon</h5>
+      <h5>* Feed the dogs</h5>
+
+
+    </div>
+  );
+}
+
+export default App;
