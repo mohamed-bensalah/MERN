eb30a2187095fc99a2f2c14d4b11b3041a6a8a71
mohamed-bensalah <118818833+mohamed-bensalah@users.noreply.github.com>
2023-01-26 09:36:31 +0100

    Prop It Up

---
+++ Week1/Day2/Practice/Prop It Up/prop/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+import PersonCard  from "./Components/PersonCard";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <PersonCard
+        lastName={"Robinson"}
+        firstName={"Joel"}
+        age={53}
+        hair={"dark brown"}
+      />
+      <PersonCard
+        lastName={"Robot"}
+        firstName={"Crow T."}
+        age={31}
+        hair={"none"}
+      />
+      <PersonCard 
+      lastName={"Servo"} 
+      firstName={"Tom"} age={31} 
+      hair={"none"} />
+      <PersonCard
+        lastName={"Frank"}
+        firstName={"TV's"}
+        age={55}
+        hair={"white"}
+      />
+    </div>
+  );
+}
+
+export default App;
